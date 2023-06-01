87426530abaa370597c51fefa0adc744e4027221
Bram Jansen <39745649+minxae@users.noreply.github.com>
2023-06-01 13:14:33 +0200

    Basic navigation  bar styling

--- jumboMaps1.0/MainPage.xaml.cs
+++ jumboMaps1.0/MainPage.xaml.cs
@@ -1,24 +1,16 @@
-﻿namespace jumboMaps1._0;
+﻿using jumboMaps1._0.ViewModel;
+
+namespace jumboMaps1._0;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
-	public MainPage()
+	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"You have clicked {count} time";
-		else
-			CounterBtn.Text = $"Wait... you have clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
---
+++ jumboMaps1.0/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace jumboMaps1._0.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string underTitle;
+
+        public MainViewModel()
+        {
+            underTitle = "Hello world";
+        }
+    }
+}
